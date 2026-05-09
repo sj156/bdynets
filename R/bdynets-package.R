7ade442b3f0b6da607db1094f8920e18b06d8972
Sheng <jiangsheng08@gmail.com>
2026-05-09 12:01:54 +0800

    Initial commit

---
+++ R/bdynets-package.R
@@ -0,0 +1,11 @@
+#' bdynets
+#'
+#' This package implements the `bdynets` method.
+#' 
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
